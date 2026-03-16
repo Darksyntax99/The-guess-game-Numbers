57644d32f360b257aaf98570acd66bf25bddda67
Darksyntax99 <darksyntax99@gmail.com>
2026-03-16 23:22:31 +0100

    star js

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    /* create a random numbers between 1 and 100 */
+ let secretgameNumber = Math.floor(Math.random() * 100) + 1;
+ /* tries number */
+ let attempts = 5;
+ 
