cda0a248d66795f8e8d022a02f5ab539db81d39e
Darksyntax99 <darksyntax99@gmail.com>
2026-03-16 23:27:49 +0100

    build js with jQuery

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
  let secretgameNumber = Math.floor(Math.random() * 100) + 1;
  /* tries number */
  let attempts = 5;
- 
+ /* game is over or not */
+ let gameisOver = false;
+ /* show the attempts number */
+    $('#attempts').text(`Attempts: ${attempts}`);
+    /* when button is clicked */
+    $("#checkBtn").click(function() {
+     if (gameisOver) return; 
+      /* get player guess */
+     let playersGuess = Number($("#guessInput").val()); 
+     /* check if player guess is valid */
+        if (playersGuess < 1 || playersGuess > 100 || isNaN(playersGuess)) {
+            $(".result").text("Enter your number between 1 and 100.");
+            return;
+        }
+        
