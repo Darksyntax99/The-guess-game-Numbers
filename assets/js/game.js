3e401c1508204e47c6ad1a88834f3d537a235455
Darksyntax99 <darksyntax99@gmail.com>
2026-03-17 12:12:49 +0100

    add jshint es6

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,7 @@
+/* jshint esversion: 6 */
+/* global $ */
 $(document).ready(function() {
-    /* create a random numbers between 1 and 100 */
+     /* create a random numbers between 1 and 100 */
  let secretgameNumber = Math.floor(Math.random() * 100) + 1;
  /* tries number */
  let attempts = 5;
